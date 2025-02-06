7cc587ce4b522ad5d7fde364ae16861a604ebfcd
Bebeto <robertohenriqueduarte05@gmail.com>
2025-02-06 16:12:40 -0300

    Envio do sql atualizado

--- banco_dados_cadastro.sql
+++ banco_dados_cadastro.sql
@@ -21,3 +21,19 @@ CREATE TABLE cadastro.uf (
 );
 
 select * from uf;
+
+-- Criação da tabela fornecedor
+CREATE TABLE fornecedor (
+  id                INT AUTO_INCREMENT NOT NULL,
+  nome              VARCHAR(100)    NOT NULL,
+  email             VARCHAR(100)    NULL,
+  endereco          VARCHAR(100)    NOT NULL,
+  uf                INT             NOT NULL,  -- chave estrangeira que referencia a tabela uf
+  telefone          VARCHAR(100)    NULL,
+  cnpj              VARCHAR(50)     NOT NULL,
+  inscricaoEstadual VARCHAR(50)     NOT NULL,
+  nomeFantasia      VARCHAR(100)    NOT NULL,
+  categoria         VARCHAR(100)    NOT NULL,
+  CONSTRAINT fornecedor_pk PRIMARY KEY (id),
+  CONSTRAINT fk_fornecedor_uf FOREIGN KEY (uf) REFERENCES uf(id)
+);
